f9912e6d157ace1965a87429827d028345c76fe0
suhas ramesh <sramesh@isravision.com>
2022-02-18 11:02:03 +0100

    Added Player and PerformanceAnalyser class

---
+++ Models/PerfomanceAnalyser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class PerfomanceAnalyser
+    {
+        double Aces { get; set; }
+        double DoubleFaults { get; set; }
+        double FirstServeInPercentage { get; set; }
+        double FirstServePointsWonPercentage {get;set;}
+        double SecondServePointsWonPercentage { get; set; }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Player
+    {
+        string Name { get; }
+
+        PerfomanceAnalyser Performace { get; set; }
+
+        double SetsWon { get; set; }
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
