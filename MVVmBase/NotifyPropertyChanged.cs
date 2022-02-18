4ac9eb4ab185754ccc929d8a966dfecf6e35c539
suhas ramesh <sramesh@isravision.com>
2022-02-18 09:47:09 +0100

    Added MVVm base

---
+++ MVVmBase/NotifyPropertyChanged.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Windows;
+
+namespace MvvmBase
+{
+    public class NotifyPropertyChanged
+   : DependencyObject
+   , INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Propertychanged
+
+        public static string GetPropertyName<T>(Expression<Func<T>> action)
+        {
+            var expression = (MemberExpression)action.Body;
+            var propertyName = expression.Member.Name;
+            return propertyName;
+        }
+
+        protected void InvokePropertyChanged<T>(Expression<Func<T>> action)
+        {
+            var propertyName = GetPropertyName(action);
+            OnPropertyChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+
+        protected bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        protected void OnExecute()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ MVVmBase/RelayCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVmBase
+{
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+
+        readonly Action<object> m_execute;
+        readonly Predicate<object> m_canExecute;
+
+        #endregion
+        #region Constructors
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            m_execute = execute;
+            m_canExecute = canExecute;
+        }
+
+        #endregion // Constructors
+
+        #region ICommand Members
+        public bool CanExecute(object parameter)
+        {
+            return m_canExecute == null ? true : m_canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            m_execute(parameter);
+        }
+
+        #endregion // ICommand Members
+    }
+}
