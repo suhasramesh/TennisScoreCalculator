c632a210094583bba562a57b0a4196137e6334a5
suhas ramesh <sramesh@isravision.com>
2022-02-18 12:29:29 +0100

    Added Spectators and Referee panel

--- TennisPageControler/ViewModels/TennisScoringViewModel.cs
+++ TennisPageControler/ViewModels/TennisScoringViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace TennisPageControler.ViewModels
 {
-    public class TennisScoringViewModel
+    public class TennisScoringViewModel: Player
     {
+        #region Properties		
+        private Player m_Player1 = new Player("Rock");
+        public Player Player1
+        {
+            get => m_Player1;
+            set
+            {
+                m_Player1 = value;
+                InvokePropertyChanged(() => Player1);
+            }
+        }
+
+        private Player m_Player2 = new Player("Sand");
+        public Player Player2
+        {
+            get => m_Player2;
+            set
+            {
+                m_Player2 = value;
+                InvokePropertyChanged(() => Player2);
+            }
+        }
+        #endregion
+        public TennisScoringViewModel()
+        {            
+        }
     }
 }
--- TennisScoreCalculator/MainWindow.xaml.cs
+++ TennisScoreCalculator/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace TennisScoreCalculator
             InitializeComponent();
             this.WindowState = WindowState.Maximized;
             DataContext = NV;
+            NV.SelectedViewModel = new TennisPageControler.Pages.TennisScoringPage();
         }
     }
 }
