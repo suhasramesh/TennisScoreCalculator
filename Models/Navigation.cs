cc582938a5a519dd25b543f2e33228be0448cf23
suhas ramesh <sramesh@isravision.com>
2022-02-18 09:52:58 +0100

    Navigation model

---
+++ Models/Navigation.cs
@@ -0,0 +1,24 @@
+﻿using MvvmBase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class NavigationModel : NotifyPropertyChanged
+    {
+        private object m_SelectedViewModel;
+        public object SelectedViewModel
+        {
+            get => m_SelectedViewModel;
+            set
+            {
+                m_SelectedViewModel = value;
+                InvokePropertyChanged(() => SelectedViewModel);
+            }
+        }
+    }
+}
