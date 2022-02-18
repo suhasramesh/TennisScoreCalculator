d68309626166ead626668175d5fec0d23dd4c988
suhas ramesh <sramesh@isravision.com>
2022-02-18 15:19:09 +0100

    Typo

--- Models/ScoreAnalyser.cs
+++ Models/ScoreAnalyser.cs
@@ -8,26 +8,5 @@ namespace Models
 {
     public class ScoreAnalyser: Player
     {
-        public const string m_PlayerName1 = "John";
-        public const string m_PlayerName2 = "Kevin";
-
-        #region Properties		
-        private IList<Player> m_Players = new List<Player>();
-        public IList<Player> Players
-        {
-            get => m_Players;
-            set
-            {
-                m_Players = value;
-                InvokePropertyChanged(() => Players);
-            }
-        }
-        #endregion
-
-        public void AddPlayers()
-        {
-            Players.Add(new Player(m_PlayerName1));
-            Players.Add(new Player(m_PlayerName2));
-        }
     }
 }
